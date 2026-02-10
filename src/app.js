b08ff58cc284a21a06eef97be229814b1a5aaabf
Maxwel Hernandez <maxwelhernandez@gmail.com>
2026-02-10 03:03:28 +0000

    random pintas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let cardarriba = document.querySelector ("#cardarriba")
+  let cardcenter = document.querySelector ("#cardcenter")
+let cardabajo = document.querySelector ("#cardabajo")
+ let pintas = ["♦", "♥", "♠","♣"]
+ let numbers = [ "A",2,3,4,5,6,7,8,9,"J","K","Q"]
+ 
+ 
+  //let randomNumbers= Math.floor(Math.random()*numbers.length)
+  //card-center.textContent = numbers [randomNumbers]
+  let randompintas = Math.floor (Math.random()*pintas.length)
+  cardarriba.textContent= pintas [randompintas]
+  cardabajo.textContent= pintas [randompintas]
+    
+    let randomNumbers = Math.floor (Math.random ()*numbers.length)
+    cardcenter.textContent= numbers [randomNumbers]
+     
+    if (pintas [randompintas] == "♦" || pintas.randompintas =="♥") {
+      cardarriba.style.color= "red"
+      cardcenter.style.color = "red"
+      cardabajo.style.color = "red"
+    }
+
+  };
